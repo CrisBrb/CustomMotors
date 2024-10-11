9dd0cba15b46b7319697d852c82bdcec616f56d5
CrisBrb <barbosacristiano124@gmail.com>
2024-10-11 05:39:59 +0200

    add test.html update style.css, style.css.map, style.scss and script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function parseCsv(csvText) {
 
 function extractRelevantData(parsedData) {
     const relevantData = [];
-    for (let i = 3; i <= 35; i++) {
+    for (let i = 3; i <= 42; i++) {
         const row = parsedData[i];
         relevantData.push({
             grade: row[1],
